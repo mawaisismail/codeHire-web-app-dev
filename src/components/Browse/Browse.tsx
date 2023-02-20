b44914bda9464815f4cfccc38b7d59a9d2835b57
Tallat <tallat_amin@hotmail.com>
2023-02-20 13:35:28 +0500

    Added Browse section below header Added some styles to the browse section

---
+++ src/components/Browse/Browse.tsx
@@ -0,0 +1,14 @@
+import styles from "./browse.module.scss";
+export const Browse = () => {
+    return (
+        <div className={styles.main}>
+            <p className={styles.p1}>
+                Browse Our <span>5,000+</span> Latest Jobs
+            </p>
+            <p className={styles.p2}>
+                Post a job to tell us about you project. we'll quickly match you with the right freelancers.
+            </p>
+            <button className={styles.button1}>Started Now 🚀</button>
+        </div>
+    );
+};
