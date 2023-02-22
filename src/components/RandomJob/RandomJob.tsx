705ff0bc34de9175950bd2c7b9f391c3f69d2c98
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-02-22 13:39:20 +0500

    Random Job

---
+++ src/components/RandomJob/RandomJob.tsx
@@ -0,0 +1,9 @@
+import styles from "./RandomJob.module.scss";
+
+export const RandomJob = () => {
+  return (
+    <div className={styles.container}>
+      <p>hello ali from the random jobs </p>
+    </div>
+  );
+};
