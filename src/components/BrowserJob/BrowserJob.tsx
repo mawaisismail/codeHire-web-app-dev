5ce7d841185b48c9ce2ddee4060f26965ec28b9c
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-02-22 13:20:42 +0500

    added changes

--- src/components/BrowserJob/BrowserJob.tsx
+++ src/components/BrowserJob/BrowserJob.tsx
@@ -1,4 +1,4 @@
-import styles from "./browser_job.module.scss";
+import styles from "./BrowserJob.module.scss";
 import { FaArrowRight } from "react-icons/fa";
 import { firstData, SecondData } from "./BrowserData";
 
@@ -6,8 +6,8 @@ export const BrowserJob = () => {
   return (
     <div className={styles.Container}>
       <div className={styles.title}>
-        <h3 className="title">Browser Jobs Categories </h3>
-        <p className="text-muted">
+        <h3>Browser Jobs Categories </h3>
+        <p>
           Post a job to tell us about your project. We will quickly match you
           with the right freelancers.
         </p>
@@ -17,8 +17,8 @@ export const BrowserJob = () => {
           return (
             <div key={data.id} className={styles.box}>
               <div className={styles.icons}>{data.icons}</div>
-              <h5 className="fs-18">{data.title}</h5>
-              <p className="text-muted mb-0">{data.content}</p>
+              <h5>{data.title}</h5>
+              <p>{data.content}</p>
             </div>
           );
         })}
@@ -28,8 +28,8 @@ export const BrowserJob = () => {
           return (
             <div key={data.id} className={styles.box}>
               <div className={styles.icons}>{data.icons}</div>
-              <h5 className="fs-18">{data.title}</h5>
-              <p className="text-muted mb-0">{data.content}</p>
+              <h5>{data.title}</h5>
+              <p>{data.content}</p>
             </div>
           );
         })}
