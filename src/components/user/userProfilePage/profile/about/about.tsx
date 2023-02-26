a4238877a7531650b0488b018b2184cdafde6b5d
Awais <muhammad.awais.77831@gmail.com>
2023-02-26 17:46:29 +0500

    fix linter

--- src/components/user/userProfilePage/profile/about/about.tsx
+++ src/components/user/userProfilePage/profile/about/about.tsx
@@ -4,13 +4,13 @@ export const About = () => {
     <div className={styles.main}>
       <p className={styles.heading}>About</p>
       <p>
-        Developer with over 5 years' experience working in both the public and
+        Developer with over 5 years experience working in both the public and
         private sectors. Diplomatic, personable, and adept at managing sensitive
         situations. Highly organized, self-motivated, and proficient with
         computers. Looking to boost students’ satisfactions scores for
-        International University. Bachelor's degree in communications.
+        International University. Bachelors degree in communications.
         <br />
-        <br /> It describes the candidate's relevant experience, skills, and
+        <br /> It describes the candidates relevant experience, skills, and
         achievements. The purpose of this career summary is to explain your
         qualifications for the job in 3-5 sentences and convince the manager to
         read the whole resume document.
