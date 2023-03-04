a09326fccd44c9777d5e622ba2eb5104ccc83d8b
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-03-04 21:59:30 +0500

    show pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,24 @@
-// import { NavComp } from "@/components/Nav_Com/NavComp";
+import { NavComp } from "@/components/Nav_Com/NavComp";
 import { Card } from "@/components/Card/Card";
-// import { Subscribe } from "@/components/Subscribe/Subscribe";
-// import { Footer } from "@/components/Footer/Footer";
+import { Subscribe } from "@/components/Subscribe/Subscribe";
+import { Footer } from "@/components/Footer/Footer";
 import { HeroSection } from "@/components/HearoSection/HeroSection";
 import { BrowserJob } from "@/components/BrowserJob/BrowserJob";
 import { JobWork } from "@/components/JobWork/JobWork";
 import { Client } from "@/components/Browse/Client";
-
+import { Slide } from "@/components/Slide/Slide";
 const Pages = () => {
   return (
     <div>
       <HeroSection />
       <BrowserJob />
-      {/*<NavComp />*/}
+      <NavComp />
       <JobWork />
       <Client />
       <Card />
-      {/*<Subscribe />*/}
-      {/*<Footer />*/}
+      <Slide />
+      <Subscribe />
+      <Footer />
     </div>
   );
 };
