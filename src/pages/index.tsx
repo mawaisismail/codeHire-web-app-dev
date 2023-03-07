51ac0eca69c098a84379cd2ff6353c30a4cc4643
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-03-07 14:29:25 +0500

    Hero Responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,9 @@
-import { NavComp } from "@/components/Nav_Com/NavComp";
-import { Card } from "@/components/Card/Card";
-import { Subscribe } from "@/components/Subscribe/Subscribe";
-import { Footer } from "@/components/Footer/Footer";
 import { HeroSection } from "@/components/HearoSection/HeroSection";
-import { BrowserJob } from "@/components/BrowserJob/BrowserJob";
-import { JobWork } from "@/components/JobWork/JobWork";
-import { Client } from "@/components/Browse/Client";
-import { Slide } from "@/components/Slide/Slide";
-import { JobNav } from "@/components/JobNavbar/JobNav";
 
 const Pages = () => {
   return (
     <div>
       <HeroSection />
-      <BrowserJob />
-      <JobNav />
-      <NavComp />
-      <JobWork />
-      <Client />
-      <Card />
-      <Slide />
-      <Subscribe />
-      <Footer />
     </div>
   );
 };
