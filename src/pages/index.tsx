c6dcd47b44eee647eeeddd2c6d8490cc92cd4019
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-03-08 03:25:09 +0500

    Browser Page Added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { HeroSection } from "@/components/HearoSection/HeroSection";
-
+import { BrowserJob } from "@/components/BrowserJob/BrowserJob";
 const Pages = () => {
   return (
     <div>
       <HeroSection />
+      <BrowserJob />
     </div>
   );
 };
