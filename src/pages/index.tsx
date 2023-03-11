9492275457752980609d95667938dab7aa037253
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-03-11 19:37:05 +0500

    JobCard added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { HeroSection } from "@/components/HearoSection/HeroSection";
 import { BrowserJob } from "@/components/BrowserJob/BrowserJob";
 import { JobNav } from "@/components/JobNavbar/JobNav";
-
+import { UserJob } from "@/components/UserJob/UserJob";
 const Pages = () => {
   return (
     <div>
       <HeroSection />
       <BrowserJob />
+      <UserJob />
       <JobNav />
     </div>
   );
