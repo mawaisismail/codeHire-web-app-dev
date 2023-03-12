583e88e8bf0b9e6039dd71a3223d42b0d5839f07
Awais <muhammad.awais.77831@gmail.com>
2023-03-12 11:11:39 +0500

    Added g-search & pagination details for re-usability

--- src/components/company/user/userSearch/UserSearch.tsx
+++ src/components/company/user/userSearch/UserSearch.tsx
@@ -2,6 +2,8 @@ import { Container } from "@mui/material";
 import styles from "./userSearch.module.scss";
 import { UserCard } from "@/components/user/UserCard/UserCard";
 import { Pagination } from "@/components/common/pagination/pagination";
+import { GSearch } from "@/components/common/search/g-search";
+import { PaginationDetails } from "@/components/common/pagination/paginationDetails/paginationDetails";
 
 export const UserSearch = () => {
   const handlePagination = async ({ selected }: { selected: number }) => {
@@ -9,16 +11,20 @@ export const UserSearch = () => {
   };
   return (
     <Container>
-      <div className={styles.main}>
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
+      <div className={styles.main_content}>
+        <GSearch />
+        <PaginationDetails pagination={100} coordinate={{ x: 10, y: 18 }} />
+        <div className={styles.main}>
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </div>
+        <Pagination handlePagination={handlePagination} pagination={100} />
       </div>
-      <Pagination handlePagination={handlePagination} pagination={100} />
     </Container>
   );
 };
