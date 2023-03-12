72828fd8b07fa6655156896214d71f70c9fb7a58
Awais <muhammad.awais.77831@gmail.com>
2023-03-12 12:21:19 +0500

    Added contact page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "@/components/HearoSection/HeroSection";
 import { BrowserJob } from "@/components/BrowserJob/BrowserJob";
 import { JobNav } from "@/components/JobNavbar/JobNav";
+import { GGetInTouch } from "@/components/common/getInTouch/g-getInTouch";
 
 const Pages = () => {
   return (
     <div>
       <HeroSection />
+      <GGetInTouch />
       <BrowserJob />
       <JobNav />
     </div>
