c7774814a913fc245d96a5252b7495e2c43d652f
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-04-18 10:30:20 -0400

    chat add

--- src/components/ChatDash/ChatDash.tsx
+++ src/components/ChatDash/ChatDash.tsx
@@ -8,3 +8,8 @@ export function ChatDash() {
   const chatProps = useMultiChatLogic(projectId, username, secret);
   return <MultiChatWindow {...chatProps} style={{ height: "100vh" }} />;
 }
+
+//
+// const projectId: string = "55dbc7e1-91be-4135-b1fb-81aae28c9e81";
+// const username: string = "ali";
+// const secret: string = "111111";
