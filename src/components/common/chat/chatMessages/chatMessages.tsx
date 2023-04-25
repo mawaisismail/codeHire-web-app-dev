bff89c015f69e737f77cf4663df1e2cda4273f85
Awais <muhammad.awais.77831@gmail.com>
2023-04-25 14:33:30 +0500

    integrate chat

---
+++ src/components/common/chat/chatMessages/chatMessages.tsx
@@ -0,0 +1,25 @@
+import { FC, Fragment, useContext } from "react";
+import { ReceivedMessage } from "../messages/chatMessages/reciverMessage/receivedMessage";
+import { SendMessage } from "../messages/chatMessages/sendMessage/sendMessage";
+import { GlobalContext } from "../../../../../utils/context/GlobalProvider";
+
+interface IChatMessages {
+  messages: { text: string; receiver: string; createdAt: string }[];
+}
+export const ChatMessages: FC<IChatMessages> = ({ messages }) => {
+  const [{ baseUser }] = useContext(GlobalContext);
+  return (
+    <Fragment>
+      {messages.length > 0 &&
+        messages.map(({ text, receiver, createdAt }, index) => (
+          <div key={`${text}-${index}`}>
+            {baseUser?.uid.toString() === receiver.toString() ? (
+              <ReceivedMessage text={text} createdAt={createdAt} />
+            ) : (
+              <SendMessage text={text} createdAt={createdAt} />
+            )}
+          </div>
+        ))}
+    </Fragment>
+  );
+};
---
+++ src/components/common/chat/messages/chatMessages/reciverMessage/receivedMessage.tsx
@@ -0,0 +1,19 @@
+import styles from "./reciverMessage.module.scss";
+import { FC } from "react";
+
+interface IUserChat {
+  text: string;
+  createdAt: string;
+}
+
+export const ReceivedMessage: FC<IUserChat> = ({ text, createdAt }) => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.user_profile}></div>
+      <p className={styles.message}>
+        {text}
+        <span>{createdAt ? createdAt : "0000 / 00 / 00 00:00"}</span>
+      </p>
+    </div>
+  );
+};
