3f588d4b534808cad62c9a7e1ff82d07aa80c2bb
omair445 <omair445@gmail.com>
2023-07-09 00:36:55 +0500

    fix build

--- src/components/user/UserMap/UserMap.tsx
+++ src/components/user/UserMap/UserMap.tsx
@@ -1,48 +1,44 @@
-import React, { useState } from 'react';
-import {
-    GoogleMap,
-    Marker,
-    useLoadScript,
-} from '@react-google-maps/api';
+import React, { useState } from "react";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 
 const Defaultcenter = { lat: 31.5204, lng: 74.3587 };
 export const UserMap = () => {
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: 'AIzaSyAWuzs6avBryDYxXo7blEkHt56ikcQ11jU',
-        libraries: ['places'],
-    });
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyAWuzs6avBryDYxXo7blEkHt56ikcQ11jU",
+    libraries: ["places"],
+  });
 
-    const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [map, setMap] = useState<google.maps.Map | null>(null);
 
-    if (loadError) {
-        return <div>Error loading maps</div>;
-    }
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
 
-    if (!isLoaded) {
-        return <div>Loading maps</div>;
-    }
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
 
-    const mapCenter = Defaultcenter;
+  const mapCenter = Defaultcenter;
 
-    return (
-        <div>
-            <GoogleMap
-                mapContainerStyle={{
-                    width: '90%',
-                    height: '200px',
-                }}
-                center={mapCenter}
-                zoom={12}
-                options={{
-                    zoomControl: true,
-                    streetViewControl: false,
-                    mapTypeControl: false,
-                    fullscreenControl: false,
-                }}
-                onLoad={(map) => setMap(map)}
-            >
-                <Marker position={mapCenter} />
-            </GoogleMap>
-        </div>
-    );
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={{
+          width: "90%",
+          height: "200px",
+        }}
+        center={mapCenter}
+        zoom={12}
+        options={{
+          zoomControl: true,
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false,
+        }}
+        onLoad={(map) => setMap(map)}
+      >
+        <Marker position={mapCenter} />
+      </GoogleMap>
+    </div>
+  );
 };
--- src/components/user/userProfilePage/profile/mainDetails/mainDetails.tsx
+++ src/components/user/userProfilePage/profile/mainDetails/mainDetails.tsx
@@ -1,7 +1,7 @@
 import styles from "./mainDetails.module.scss";
 import { FaStar } from "react-icons/fa";
 import { Documents } from "@/components/user/userProfilePage/profile/documents/documents";
-import { UserLocation } from "@/components/user/userProfilePage/profile/UserLocation/UserLocation"
+import { UserLocation } from "@/components/user/userProfilePage/profile/UserLocation/UserLocation";
 import { useContext } from "react";
 import { GlobalContext } from "../../../../../../utils/context/GlobalProvider";
 
@@ -45,7 +45,7 @@ export const MainDetails = () => {
           </div>
         </div>
       </div>
-      <UserLocation/>
+      <UserLocation />
     </div>
   );
 };
