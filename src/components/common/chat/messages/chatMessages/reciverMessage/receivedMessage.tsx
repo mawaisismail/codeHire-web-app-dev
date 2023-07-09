b3d21d1d06974157ed3e479b8374ea5f5a68af27
Muhammad Ali Yousaf <m.aliyousaf1133@gmail.com>
2023-07-09 14:00:40 -0400

    chat design

--- src/components/common/chat/messages/chatMessages/reciverMessage/receivedMessage.tsx
+++ src/components/common/chat/messages/chatMessages/reciverMessage/receivedMessage.tsx
@@ -2,19 +2,20 @@ import styles from "./reciverMessage.module.scss";
 import { FC } from "react";
 
 interface IUserChat {
-  text: string;
-  createdAt: string;
+    text: string;
+    createdAt: string;
 }
 
 export const ReceivedMessage: FC<IUserChat> = ({ text, createdAt }) => {
-  return (
-    <div className={styles.main}>
-      <div className={styles.user_profile}></div>
-      <p className={styles.message}>
-        {text}
-        <span>{createdAt ? createdAt : "0000 / 00 / 00 00:00"}</span>
-        <span>helooooooooooo</span>
-      </p>
-    </div>
-  );
+    const formattedTime = createdAt ? new Date(createdAt).toLocaleTimeString() : "00:00:00";
+
+    return (
+        <div className={styles.main}>
+            <div className={styles.user_profile}></div>
+            <p className={styles.message}>
+                {text}
+                <span>{formattedTime}</span>
+            </p>
+        </div>
+    );
 };
